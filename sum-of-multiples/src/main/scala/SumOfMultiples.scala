257e68303a404f3de9eef97ca514800a9a85eed7
Theemathas Chirananthavat <theemathas@gmail.com>
2018-03-15 12:27:10 -0700

    Solve the sum-of-multiples exercise.

--- sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,4 +1,7 @@
 object SumOfMultiples {
-  def sum(factors: Set[Int], limit: Int): Int = ???
+  // Returns the sum of integers in 1,2,...,limit that are divisible by at least
+  // one of the numbers in factors
+  def sum(factors: Set[Int], limit: Int): Int =
+    (1 until limit).filter(x => factors.exists(x % _ == 0)).sum
 }
 
