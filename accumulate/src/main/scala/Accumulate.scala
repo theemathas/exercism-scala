33d660fa13fb1d18f83c2518f3d78c499c2c6c13
Theemathas Chirananthavat <theemathas@gmail.com>
2018-03-16 16:45:07 -0700

    Solve the accumulate exercise.

--- accumulate/src/main/scala/Accumulate.scala
+++ accumulate/src/main/scala/Accumulate.scala
@@ -1,3 +1,8 @@
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = ???
+  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
+    list match {
+      case Nil => Nil
+      case h :: t => f(h) :: accumulate(f, t)
+    }
+  }
 }
