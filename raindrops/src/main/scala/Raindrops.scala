93c6bbd24695102a7bbe4953b955ac6bc589a665
Theemathas Chirananthavat <theemathas@gmail.com>
2018-03-21 00:49:12 -0700

    Solve the raindrops exercise.

--- raindrops/src/main/scala/Raindrops.scala
+++ raindrops/src/main/scala/Raindrops.scala
@@ -1,4 +1,16 @@
 object Raindrops {
-  def convert(n: Int): String = ???
+  def convert(n: Int): String = {
+    val str = new StringBuilder
+    if (n % 3 == 0) {
+      str.append("Pling")
+    }
+    if (n % 5 == 0) {
+      str.append("Plang")
+    }
+    if (n % 7 == 0) {
+      str.append("Plong")
+    }
+    if (str.isEmpty) n.toString else str.toString
+  }
 }
 
