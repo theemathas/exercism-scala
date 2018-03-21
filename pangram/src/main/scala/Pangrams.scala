31ed2d755d5e4a6aadcb80a6b28da3f6d8aa0a9b
Theemathas Chirananthavat <theemathas@gmail.com>
2018-03-20 18:55:28 -0700

    Solve the pangram exercise.

--- pangram/src/main/scala/Pangrams.scala
+++ pangram/src/main/scala/Pangrams.scala
@@ -1,4 +1,5 @@
 object Pangrams {
-  def isPangram(input: String): Boolean = ???
+  def isPangram(input: String): Boolean =
+    input.filter(_.isLetter).toLowerCase.toSet == ('a' to 'z').toSet
 }
 
