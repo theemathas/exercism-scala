803f11c8ef7c4b45eed52f5954ae0b618e0b1e4e
Theemathas Chirananthavat <theemathas@gmail.com>
2018-03-22 00:37:49 -0700

    Solve the rn-transcription exercise.

--- rna-transcription/src/main/scala/RnaTranscription.scala
+++ rna-transcription/src/main/scala/RnaTranscription.scala
@@ -1,3 +1,9 @@
 object RnaTranscription {
-
+  def toRna(dna: String): Option[String] = Some(dna.map {
+    case 'G' => 'C'
+    case 'C' => 'G'
+    case 'T' => 'A'
+    case 'A' => 'U'
+    case _ => return None
+  })
 }
