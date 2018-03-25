18652d897159e2af7fde3823bd97478d1c95267a
Theemathas Chirananthavat <theemathas@gmail.com>
2018-03-24 20:27:10 -0700

    Solve the meetup exercise.

--- meetup/src/main/scala/Meetup.scala
+++ meetup/src/main/scala/Meetup.scala
@@ -1,10 +1,28 @@
-import java.time.{DayOfWeek, LocalDate}
+import java.time.{DayOfWeek, LocalDate, YearMonth}
 
 import Schedule.Schedule
 
 case class Meetup(month: Int, year: Int) {
 
-  def day(dayOfWeek: Int, schedule: Schedule): LocalDate = ???
+  def day(dayOfWeek: DayOfWeek, schedule: Schedule): LocalDate = {
+    val days = allDays(dayOfWeek)
+    schedule match {
+      case Schedule.Teenth =>
+        days.find(date => (13 to 19).contains(date.getDayOfMonth)).get
+      case Schedule.First  => days(0)
+      case Schedule.Second => days(1)
+      case Schedule.Third  => days(2)
+      case Schedule.Fourth => days(3)
+      case Schedule.Last => days.last
+    }
+  }
+
+  /** All days in the month with the given dayOfWeek */
+  private def allDays(dayOfWeek: DayOfWeek): Seq[LocalDate] = {
+    (1 to YearMonth.of(year, month).lengthOfMonth)
+      .map(LocalDate.of(year, month, _))
+      .filter(_.getDayOfWeek == dayOfWeek)
+  }
 }
 
 object Schedule extends Enumeration {
@@ -13,11 +31,11 @@ object Schedule extends Enumeration {
 }
 
 object Meetup {
-  val Mon = DayOfWeek.MONDAY.getValue
-  val Tue = DayOfWeek.TUESDAY.getValue
-  val Wed = DayOfWeek.WEDNESDAY.getValue
-  val Thu = DayOfWeek.THURSDAY.getValue
-  val Fri = DayOfWeek.FRIDAY.getValue
-  val Sat = DayOfWeek.SATURDAY.getValue
-  val Sun = DayOfWeek.SUNDAY.getValue
+  val Mon = DayOfWeek.MONDAY
+  val Tue = DayOfWeek.TUESDAY
+  val Wed = DayOfWeek.WEDNESDAY
+  val Thu = DayOfWeek.THURSDAY
+  val Fri = DayOfWeek.FRIDAY
+  val Sat = DayOfWeek.SATURDAY
+  val Sun = DayOfWeek.SUNDAY
 }
